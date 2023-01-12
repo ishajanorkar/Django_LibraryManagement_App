7f653e4021873474a7242d3a2618ee1f6313d670
Isha <ishajanorkar4691@gmail.com>
2023-01-12 10:31:27 +0530

    first commit

--- library/settings.py
+++ library/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oiipxnuf--igw)6le_-l-*xz+w)ssd*&=b0%@*=wz2!)16$ian'
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -77,10 +78,10 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'libbrarry',
-        'HOST':'localhost',
-        'USER':'root',
+        'ENGINE': '',
+        'NAME':'',
+        'HOST':'',
+        'USER':'',
         'PASSWORD':''
     }
 }
